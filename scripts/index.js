403d59db573c986463ac510d599b99edbcb7113d
Valentinochka78 <140449463+Valentinochka78@users.noreply.github.com>
2024-06-23 23:47:30 +0300

    правим код

--- scripts/index.js
+++ scripts/index.js
@@ -6,12 +6,12 @@ const content = document.querySelector('.content');
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard (element, removeCard) {
+function createCard (cardData, removeCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__title').textContent = element.name;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
     const cardImage = cardElement.querySelector('.card__image');
-    cardImage.src = element.link;
-    cardImage.alt = element.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
   const deleteButton = cardElement.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', removeCard);
       return cardElement;
