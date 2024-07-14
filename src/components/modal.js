a5da677f0c835bcfbe926e2d4bd5986717e940b2
Valentinochka78 <140449463+Valentinochka78@users.noreply.github.com>
2024-07-14 21:00:56 +0300

    помогите,что дальше

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,31 +1,18 @@
-export function closeModal(modal) {
-    modal.classList.remove("popup_is-opened");
-    document.removeEventListener("keydown", closeOnEsc);
+function openModal(popup) {
+  popup.classList.add("popup_is-opened");
+  document.addEventListener("keydown", handleEscClose);
+}
+
+function closeModal(popup) {
+  popup.classList.remove("popup_is-opened");
+  document.removeEventListener("keydown", handleEscClose);
+}
+
+function handleEscClose(evt) {
+  if (evt.key === "Escape") {
+    const popupOpened = document.querySelector(".popup_is-opened");
+    closeModal(popupOpened);
   }
-  
-  export function openModal(modal) {
-    modal.classList.add("popup_is-opened");
-    document.addEventListener("keydown", closeOnEsc);
-  }
-  
-  function closeOnEsc(event) {
-    if (event.key === "Escape") {
-      const openedPopup = document.querySelector(".popup_is-opened");
-      closeModal(openedPopup);
-    }
-  }
-  
-  function closePopupHandler(popup) {
-    return function (event) {
-      if (event.target === popup || event.target.classList.contains("popup__close")) {
-        closeModal(popup);
-      }
-    };
-  }
-  
-  export function setEventListeners () {
-    const popups = document.querySelectorAll(".popup"); 
-    popups.forEach(function(popup) {
-      popup.addEventListener("click", closePopupHandler(popup));
-    });
-  } 
+}
+
+export { openModal, closeModal };
